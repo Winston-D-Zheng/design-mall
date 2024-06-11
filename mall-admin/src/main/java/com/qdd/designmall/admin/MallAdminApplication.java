6c685410fafc2b3b4cd15a29e349ce73e4c125c0
wizard <105677765@qq.com>
2024-06-11 21:43:17 +0800

    first optimize import

--- mall-admin/src/main/java/com/qdd/designmall/admin/MallAdminApplication.java
+++ mall-admin/src/main/java/com/qdd/designmall/admin/MallAdminApplication.java
@@ -1,12 +1,7 @@
 package com.qdd.designmall.admin;
 
-import com.qdd.designmall.common.config.property.QiniuyunProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.Map;
 
 @SpringBootApplication(scanBasePackages = "com.qdd.designmall")
 public class MallAdminApplication {
--- mall-mbp/src/main/java/com/qdd/designmall/mbp/model/DbHmsPromotionShop.java
+++ mall-mbp/src/main/java/com/qdd/designmall/mbp/model/DbHmsPromotionShop.java
@@ -1,15 +1,12 @@
 package com.qdd.designmall.mbp.model;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
-import java.util.Date;
-
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.qdd.designmall.common.serializer.PicUrlSerializer;
 import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @TableName db_hms_promotion_shop
